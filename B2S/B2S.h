d01c83ae7e2c5ce40a4ec6ba5873f2f63ff677d4
Egor Sakhabaev <sakhabaevegor@gmail.com>
2021-05-26 11:44:27 +0700

    Initial Commit

---
+++ B2S/B2S.h
@@ -0,0 +1,18 @@
+//
+//  B2S.h
+//  B2S
+//
+//  Created by Egor Sakhabaev on 26.05.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for B2S.
+FOUNDATION_EXPORT double B2SVersionNumber;
+
+//! Project version string for B2S.
+FOUNDATION_EXPORT const unsigned char B2SVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <B2S/PublicHeader.h>
+
+
